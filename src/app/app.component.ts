e881bd8a57d3ce6d18e4e488fa429006aa97fb1b
Hermes Hostin <hermeshostin@Peves-MacBook-Pro.local>
2023-11-18 15:59:45 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,30 @@ import { RouterModule } from '@angular/router';
 import { AuthService } from '@lib/services';
 import { ThemeService } from '@lib/services/theme';
 import { LayoutHorizontalComponent } from './lib/components/layouts/layout-horizontal/layout-horizontal.component';
+import { HttpClientModule } from '@angular/common/http';
+import {LOCALE_ID, DEFAULT_CURRENCY_CODE} from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localePt, 'pt');
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterModule, LayoutHorizontalComponent],
+    imports: [CommonModule, RouterModule, LayoutHorizontalComponent, HttpClientModule],
+    providers: [
+        {
+            provide: LOCALE_ID,
+            useValue: 'pt'
+        },
+
+        /* if you don't provide the currency symbol in the pipe, 
+        this is going to be the default symbol (R$) ... */
+        {
+            provide:  DEFAULT_CURRENCY_CODE,
+            useValue: 'BRL'
+        },
+    ],
     templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppTheme, ThemeService } from '@lib/services/theme';
 import { Subject, takeUntil } from 'rxjs';
+
 
 @Component({
     standalone: true,
@@ -16,10 +18,59 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     private readonly _destroy$ = new Subject();
 
+    tickersDividendHistory: any[] = []
+    tickers = ["BBAS3","TRPL4", "TAEE4", "TAEE11", "BBSE3"]
+
+
+    constructor(private http: HttpClient) { }
+
     ngOnInit(): void {
         this._themeService.currentTheme$
             .pipe(takeUntil(this._destroy$))
             .subscribe((theme) => (this.currentTheme = theme));
+        this.getTickersInfo();
+       
+
+        
+    }
+
+    async getTickersInfo() {
+        this.tickers.forEach((ticker) => {
+
+            this.http.get<any>(`http://localhost:4200/api/acao/companytickerprovents?ticker=${ticker}&chartProventsType=2`).subscribe(data => {
+                console.log(ticker);
+                console.log(data);
+                this.calculePrice(data);
+                this.setActualValue(data, ticker)
+                this.tickersDividendHistory.push({ ticker: ticker, history: data });
+            });
+        });
+    }
+    setActualValue(data: any, ticker: string) {
+        this.http.get<any>(`http://localhost:4200/apihome/mainsearchquery?q=${ticker}`).subscribe(response => {
+                console.log(ticker);
+                console.log(response);
+                data.actualPrice = response[0].price
+            });
+    }
+
+    calculePrice(data: any) {
+        let totalValue = 0
+        console.log(new Date().getFullYear())
+        console.log(new Date().getFullYear() - 6)
+        const years = data.assetEarningsYearlyModels
+        .filter((year:any) => (year.rank < new Date().getFullYear() && year.rank >= (new Date().getFullYear() - 6)))
+        years.forEach((year: any) => {
+            console.log(year.rank + ":" + year.value)
+            totalValue += year.value
+        })
+        console.log(totalValue)
+        console.log(years.length)
+        const averageDividendValue = totalValue / (years.length)
+        console.log(averageDividendValue)
+        const price = averageDividendValue / 0.06
+        data.price = price
+  
     }
 
     ngOnDestroy(): void {
